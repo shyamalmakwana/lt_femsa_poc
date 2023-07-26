0e0eb132583c543a675d4ccaec90b8c548a8e678
Shyamal Makwana <makwanashyamal96@gmail.com>
2023-07-26 13:38:45 +0000

    updated codecept.conf.js

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,4 +1,5 @@
 const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+const { Options } = require('selenium-webdriver');
 setHeadlessWhen(process.env.HEADLESS);
 setCommonPlugins();
 
@@ -6,22 +7,19 @@ setCommonPlugins();
 exports.config = {
   tests: './*_test.js',
   output: './output',
+
   helpers: {
     Appium: {
-      host: "https://webhook.site/893da154-b563-4dd9-bfda-f1dacbbc8d68",
-      port: 4444,
+      host: "mobile-hub.lambdatest.com",
+      path: "/wd/hub/",
+      port: 80,
+      platform: "android",
       user: process.env.LT_USERNAME,
       key: process.env.LT_ACCESS_KEY,
-      app: "lt://APP10160631101690289414695666",
-      desiredCapabilities: {
-        platformName: "android",
-        app: "lt://APP10160631101690289414695666",
-        deviceName: "Pixel 6",
-        platformVersion: "12",
-        isRealMobile: true,
-        //appPackage: 'com.wdiodemoapp',
-        //appActivity: 'com.wdiodemoapp.SplashActivity',
-        //automationName: 'UiAutomator2'
+      desiredCapabilities: {         
+          deviceName: "Galaxy.*",
+          app: "lt://APP10160631101690377303988307",
+          isRealMobile: true
       }
     }
   },
